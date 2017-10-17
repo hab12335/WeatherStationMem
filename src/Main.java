464157662522141cd181a99eb54036607eaf45f2
Hugo Botas <hab12335@campus.fct.unl.pt>
2017-10-17 23:30:44 +0100

    Depois da fac, tudo testado

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        WeatherStationMem temperaturas = new WeatherStationMem();
+        temperaturas.sampleTemperature(66);
+        temperaturas.sampleTemperature(30);
+        for (double i = 0; i < 3; i++) {
+            double random = -10 + Math.random() * (45 + 10);
+            temperaturas.sampleTemperature(random);
+        }
+        temperaturas.countSamplesGreaterThan(10);
+        temperaturas.countSamplesLowerThan(20);
+        temperaturas.insertAt(66, 5);
+        System.out.println(temperaturas.firstIndexOfSample(66));
+        System.out.println(temperaturas.lastIndexOfSample(66));
     }
 }
--- src/WeatherStationMem.java
+++ src/WeatherStationMem.java
@@ -1,6 +1,7 @@
 public class WeatherStationMem {
     private int counter;
     public static final int DEFAULT_SIZE = 100;
+    public static final int GROWTH = 2;
     private double[] samples;
 
     public WeatherStationMem() {
@@ -8,8 +9,15 @@ public class WeatherStationMem {
         samples = new double[DEFAULT_SIZE];
     }
 
+
     public void sampleTemperature(double temp) {
+        if (this.atCapacity())
+            this.resize();
         samples[counter++] = temp;
+    }
+
+    private boolean atCapacity() {
+        return counter == samples.length;
     }
 
     public int numberTemperatures() {
@@ -29,15 +37,79 @@ public class WeatherStationMem {
     }
 
     public double maximum() {
-        int i= 0;
-        double sum = 0;
+        double max = 0;
+        for (int i = 0; i < counter; i++)
+            if (samples[i] > max)
+                max = samples[i];
+        return max;
+    }
+
+    public double minimum() {
+        double min = samples[0];
+        for (int i = 0; i < counter; i++)
+            if (samples[i] < min)
+                min = samples[i];
+        return min;
+    }
+
+    private void resize() {
+        double[] tmp = new double[GROWTH * samples.length];
+        for (int i = 0; i < samples.length; i++)
+            tmp[i] = samples[i];
+        samples = tmp;
+    }
+
+    public int firstIndexOfSample(double temp) {
+        int result = -1;
+        for (int i = 0; i < counter && result == -1; i++)
+            if (samples[i] == temp)
+                result = i;
+        return result;
+    }
+
+    public int lastIndexOfSample(double temp) {
+        int result = -1;
+        for (int i = counter; i >= 0 && result == -1; i--)
+            if (samples[i] == temp)
+                result = i;
+        return result;
 
     }
 
-    public double minumum() {
-        int i= 0;
-        double sum = 0;
-
+    public int countSamplesGreaterThan(double temp) {
+        int innerCounter = 0;
+        for (int i = 0; i < counter; i++) {
+            if (samples[i] > temp)
+                innerCounter++;
+        }
+        return innerCounter;
     }
 
+    public int countSamplesLowerThan(double temp) {
+        int innerCounter = 0;
+        for (int i = 0; i < counter; i++) {
+            if (samples[i] < temp)
+                innerCounter++;
+        }
+        return innerCounter;
+    }
+
+    public void insertAt(double temp, int pos) {
+        if (this.atCapacity())
+            this.resize();
+        for (int i = counter; i >= pos ; i--) {
+            if (i != pos)
+                samples[i] = samples[i - 1];
+            else
+                samples[i] = temp;
+        }
+        counter++;
+    }
+
+    public void removeAt(int pos) {
+        for (int i = pos; i < counter ; i++) {
+            samples[i] = samples[i + 1];
+        }
+        counter--;
+    }
 }
